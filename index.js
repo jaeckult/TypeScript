3d9e424c90888b71138697775dc7cdbdeed4d3d4
root <maj583601@gmail.com>
2025-02-10 17:46:30 -0500

    lab_1

---
+++ index.js
@@ -0,0 +1,34 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var restaurants_1 = require("./restaurants");
+var dollarSigns = '$$';
+var deliveryTimeMax = 90;
+var maxDistance = '10';
+var result;
+var hour = new Date().getHours();
+var priceBracket = dollarSigns.length;
+var filteredRestaurants = restaurants_1.default.filter(function (restaurant) {
+    if (Number(restaurant.priceBracket) > priceBracket) {
+        return false;
+    }
+    if (restaurant.deliveryTimeMinutes > deliveryTimeMax) {
+        return false;
+    }
+    if (restaurant.distance > maxDistance) {
+        return false;
+    }
+    if (Number(restaurant.openHour) > hour) {
+        return false;
+    }
+    if (Number(restaurant.closeHour) < hour) {
+        return false;
+    }
+    return restaurant;
+});
+if (filteredRestaurants.length === 0) {
+    result = 'There are no restaurants available right now.';
+}
+else {
+    result = "We found ".concat(filteredRestaurants.length, " restaurants, the first is ").concat(filteredRestaurants[0].name, ".");
+}
+console.log(result);
